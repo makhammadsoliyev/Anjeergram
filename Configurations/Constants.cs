05dd95495993b0c3446c6c372956f8844039f43b
makhammadsoliyev <makhammadsoliyev@gmail.com>
2024-02-10 14:25:33 +0500

    Add Configurations

---
+++ Configurations/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace Anjeergram.Configurations;
+
+public static class Constants
+{
+    public const string POSTS_PATH = @"..\..\..\..\Anjeergram\Database\posts.json";
+    public const string USERS_PATH = @"..\..\..\..\Anjeergram\Database\users.json";
+    public const string MESSAGES_PATH = @"..\..\..\..\Anjeergram\Database\messages.json";
+    public const string COMMENTS_PATH = @"..\..\..\..\Anjeergram\Database\comments.json";
+    public const string POST_LIKES_PATH = @"..\..\..\..\Anjeergram\Database\post_likes.json";
+    public const string COMMENT_LIKES_PATH = @"..\..\..\..\Anjeergram\Database\comment_likes.json";
+}
